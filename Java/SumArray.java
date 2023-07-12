a365be62229465afdd5dd61721e766cc45665a53
Eric Separovic <ericseparovic07@gmail.com>
2023-07-12 08:35:35 -0300

    add comment SumArray

--- Java/SumArray.java
+++ Java/SumArray.java
@@ -4,23 +4,32 @@
 
 public class SumArray {
     public static void main(String[] args) {
+        // Declares and initializes an array of double numbers with three elements.
         double[] numbers = {1.1, 2.2, 3.3};
+        // Calls the "sum" method passing the "numbers" array and assigns the returned value to the "result" variable.
         double result = sum(numbers);
 
+        // Prints the value of "result" to the console.
         System.out.println(result);
     }
 
+    // Calculates the sum of the numbers in the given array.
     public static double sum(double[] numbers){
+        // Declares and initializes a variable "total" to store the sum of the numbers.
         double total = 0;
 
+        // Checks if the array "numbers" is empty.
         if(numbers.length == 0){
+            // If the array is empty, returns 0.
             return total;
         }
 
-        for(int i = 0; i < numbers.length;i++){
+        // Iterates over each element in the "numbers" array and adds it to the "total".
+        for(int i = 0; i < numbers.length; i++){
             total = numbers[i] + total;
         }
 
+        // Returns the calculated total sum.
         return total;
     }
 }
